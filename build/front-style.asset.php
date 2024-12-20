e99a737fa579d955233adc87e4ae58eae2e7ccc7
Cobianzo <cobianzo@gmail.com>
2024-12-20 18:43:36 +0100

    Adding distribution zip on 2024-12-20 18:43

--- build/front-style.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '320588bab80085c992e2');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-i18n'), 'version' => '8d0c5bc04dc7f2262f35');
